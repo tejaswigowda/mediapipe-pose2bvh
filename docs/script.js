20e45318ec4ad248f39b6f05f4f230b1901f184d
Tejaswi Gowda <tejaswi@asu.edu>
2024-01-16 11:56:12 -0700

    ac

--- docs/script.js
+++ docs/script.js
@@ -28,18 +28,16 @@ function testSupport(supportedDevices) {
     }
     if (!isSupported) {
         alert(`This demo, running on ${detectedDevice.client.name}/${detectedDevice.os.name}, ` +
-            `is not well supported at this time, expect some flakiness while we improve our code.`);
+            `is not well supported at this time, continue at your own risk.`);
     }
 }
 const controls = window;
-const LandmarkGrid = window.LandmarkGrid;
+const mpHolistic = window;
 const drawingUtils = window;
-const mpPose = window;
-const options = {
-    locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${mpPose.VERSION}/${file}`;
-    }
-};
+const config = { locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic@` +
+            `${mpHolistic.VERSION}/${file}`;
+    } };
 // Our input frames will come from here.
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
@@ -53,23 +51,39 @@ const spinner = document.querySelector('.loading');
 spinner.ontransitionend = () => {
     spinner.style.display = 'none';
 };
-const landmarkContainer = document.getElementsByClassName('landmark-grid-container')[0];
-const grid = new LandmarkGrid(landmarkContainer, {
-    connectionColor: 0xCCCCCC,
-    definedColors: [{ name: 'LEFT', value: 0xffa500 }, { name: 'RIGHT', value: 0x00ffff }],
-    range: 2,
-    fitToGrid: true,
-    labelSuffix: 'm',
-    landmarkSize: 2,
-    numCellsPerAxis: 4,
-    showHidden: false,
-    centered: true,
-});
+function removeElements(landmarks, elements) {
+    for (const element of elements) {
+        delete landmarks[element];
+    }
+}
+function removeLandmarks(results) {
+    if (results.poseLandmarks) {
+        removeElements(results.poseLandmarks, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20, 21, 22]);
+    }
+}
+function connect(ctx, connectors) {
+    const canvas = ctx.canvas;
+    for (const connector of connectors) {
+        const from = connector[0];
+        const to = connector[1];
+        if (from && to) {
+            if (from.visibility && to.visibility &&
+                (from.visibility < 0.1 || to.visibility < 0.1)) {
+                continue;
+            }
+            ctx.beginPath();
+            ctx.moveTo(from.x * canvas.width, from.y * canvas.height);
+            ctx.lineTo(to.x * canvas.width, to.y * canvas.height);
+            ctx.stroke();
+        }
+    }
+}
 let activeEffect = 'mask';
 function onResults(results) {
-    processResults(results);
     // Hide the spinner.
     document.body.classList.add('loaded');
+    // Remove landmarks we don't want to draw.
+    removeLandmarks(results);
     // Update the frame rate.
     fpsControl.tick();
     // Draw the overlays.
@@ -97,28 +111,62 @@ function onResults(results) {
     else {
         canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
     }
+    // Connect elbows to hands. Do this first so that the other graphics will draw
+    // on top of these marks.
+    canvasCtx.lineWidth = 5;
     if (results.poseLandmarks) {
-        drawingUtils.drawConnectors(canvasCtx, results.poseLandmarks, mpPose.POSE_CONNECTIONS, { visibilityMin: 0.65, color: 'white' });
-        drawingUtils.drawLandmarks(canvasCtx, Object.values(mpPose.POSE_LANDMARKS_LEFT)
-            .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'rgb(255,138,0)' });
-        drawingUtils.drawLandmarks(canvasCtx, Object.values(mpPose.POSE_LANDMARKS_RIGHT)
-            .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'rgb(0,217,231)' });
-        drawingUtils.drawLandmarks(canvasCtx, Object.values(mpPose.POSE_LANDMARKS_NEUTRAL)
-            .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'white' });
+        if (results.rightHandLandmarks) {
+            canvasCtx.strokeStyle = 'white';
+            connect(canvasCtx, [[
+                    results.poseLandmarks[mpHolistic.POSE_LANDMARKS.RIGHT_ELBOW],
+                    results.rightHandLandmarks[0]
+                ]]);
+        }
+        if (results.leftHandLandmarks) {
+            canvasCtx.strokeStyle = 'white';
+            connect(canvasCtx, [[
+                    results.poseLandmarks[mpHolistic.POSE_LANDMARKS.LEFT_ELBOW],
+                    results.leftHandLandmarks[0]
+                ]]);
+        }
     }
+    // Pose...
+    drawingUtils.drawConnectors(canvasCtx, results.poseLandmarks, mpHolistic.POSE_CONNECTIONS, { color: 'white' });
+    drawingUtils.drawLandmarks(canvasCtx, Object.values(mpHolistic.POSE_LANDMARKS_LEFT)
+        .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'rgb(255,138,0)' });
+    drawingUtils.drawLandmarks(canvasCtx, Object.values(mpHolistic.POSE_LANDMARKS_RIGHT)
+        .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'rgb(0,217,231)' });
+    // Hands...
+    drawingUtils.drawConnectors(canvasCtx, results.rightHandLandmarks, mpHolistic.HAND_CONNECTIONS, { color: 'white' });
+    drawingUtils.drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+        color: 'white',
+        fillColor: 'rgb(0,217,231)',
+        lineWidth: 2,
+        radius: (data) => {
+            return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
+        }
+    });
+    drawingUtils.drawConnectors(canvasCtx, results.leftHandLandmarks, mpHolistic.HAND_CONNECTIONS, { color: 'white' });
+    drawingUtils.drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+        color: 'white',
+        fillColor: 'rgb(255,138,0)',
+        lineWidth: 2,
+        radius: (data) => {
+            return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
+        }
+    });
+    // Face...
+    drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_TESSELATION, { color: '#C0C0C070', lineWidth: 1 });
+    drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_RIGHT_EYE, { color: 'rgb(0,217,231)' });
+    drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_RIGHT_EYEBROW, { color: 'rgb(0,217,231)' });
+    drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_LEFT_EYE, { color: 'rgb(255,138,0)' });
+    drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_LEFT_EYEBROW, { color: 'rgb(255,138,0)' });
+    drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_FACE_OVAL, { color: '#E0E0E0', lineWidth: 5 });
+    drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, mpHolistic.FACEMESH_LIPS, { color: '#E0E0E0', lineWidth: 5 });
     canvasCtx.restore();
-    if (results.poseWorldLandmarks) {
-        grid.updateLandmarks(results.poseWorldLandmarks, mpPose.POSE_CONNECTIONS, [
-            { list: Object.values(mpPose.POSE_LANDMARKS_LEFT), color: 'LEFT' },
-            { list: Object.values(mpPose.POSE_LANDMARKS_RIGHT), color: 'RIGHT' },
-        ]);
-    }
-    else {
-        grid.updateLandmarks([]);
-    }
 }
-const pose = new mpPose.Pose(options);
-pose.onResults(onResults);
+const holistic = new mpHolistic.Holistic(config);
+holistic.onResults(onResults);
 // Present a control panel through which the user can manipulate the solution
 // options.
 new controls
@@ -133,14 +181,14 @@ new controls
     effect: 'background',
 })
     .add([
-    new controls.StaticText({ title: 'MediaPipe Pose' }),
+    new controls.StaticText({ title: 'MediaPipe Holistic' }),
     fpsControl,
     new controls.Toggle({ title: 'Selfie Mode', field: 'selfieMode' }),
     new controls.SourcePicker({
         onSourceChanged: () => {
-            // Resets because this model gives better results when reset between
+            // Resets because the pose gives better results when reset between
             // source changes.
-            pose.reset();
+            holistic.reset();
         },
         onFrame: async (input, size) => {
             const aspect = size.height / size.width;
@@ -155,7 +203,7 @@ new controls
             }
             canvasElement.width = width;
             canvasElement.height = height;
-            await pose.send({ image: input });
+            await holistic.send({ image: input });
         },
     }),
     new controls.Slider({
@@ -188,5 +236,5 @@ new controls
     const options = x;
     videoElement.classList.toggle('selfie', options.selfieMode);
     activeEffect = x['effect'];
-    pose.setOptions(options);
+    holistic.setOptions(options);
 });
